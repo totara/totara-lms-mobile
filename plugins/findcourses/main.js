b60fb1f1d7f665cab4a0f82a62d3f7ff305bed33
Tony Blundell <tony.blundell@kineo.com>
2013-11-11 16:49:59 +0000

    MOBILE-437 WIP

---
+++ plugins/findcourses/main.js
@@ -0,0 +1,190 @@
+var requires = [
+    "root/externallib/text!root/plugins/findcourses/courses.html"
+];
+
+define(requires, function(coursesTpl) {
+
+    var callbacks = {
+        collate_categories_and_courses: function(data) {
+            var missing_categories = false;
+            var categories = _.reduceRight(data, function(memo, course) {
+                if (course.categoryid === 0) return memo;
+
+                var category = MM.db.get(
+                    'categories', course.categoryid
+                );
+                if (category !== undefined) {
+                    var categoryName = category.get('name');
+
+                    if (memo[categoryName] === undefined){
+                        memo[categoryName] = [];
+                    }
+
+                    // Has the user already started this course?
+                    // Course id is mildly contrived, but stops collisions between
+                    // sites.
+                    var courseId = MM.config.current_site.id + '-' + course.id;
+                    if (MM.db.exists("mylearning")) {
+                        course.started = MM.db.get('mylearning', courseId) !== undefined;
+                    } else {
+                        course.started = false;
+                    }
+                    course.started = false;
+
+                    memo[categoryName].push(course);
+                } else {
+                    missing_categories = true;
+                }
+
+                return memo;
+            }, {});
+
+            if (missing_categories === true) {
+                categories = false;
+            }
+
+            return categories;
+        },
+
+        find_courses_successful:function(data) {
+            var categories = callbacks.collate_categories_and_courses(data);
+
+            if (categories !== false) {
+                var values = {
+                    'categories' : categories
+                };
+                var html = MM.tpl.render(
+                    MM.plugins.findcourses.templates.results.html, values, {}
+                );
+
+                MM.panels.show('center', html, {hideRight: false});
+            } else {
+                // Record these results - they're still valid.
+                MM.plugins.findcourses.last_found_courses = data;
+
+                // Go and get the refreshed list of categories.
+                // This goes back to find_categories_successful.
+                MM.plugins.findcourses.list_categories();
+            }
+        },
+
+        find_categories_successful: function(data) {
+            // Store the categories in the app.
+            // Categories are stored against the site id.
+            _.each(data, function(category) {
+                MM.db.insert('categories', category)
+            });
+
+            plugin.find_courses();
+        },
+
+        manual_enrolment_successful: function() {
+            // Change the url, don't actually navigate to it though.
+            MM.Router.navigate("#find_courses", {'trigger':false});
+
+            var courseId = MM.config.current_site.id + '-' + MM.plugins.plugin.last_enrolled_course;
+
+            // Take the last enrolled course and set it as 'started'
+            MM.db.update(
+                "courses",
+                courseId,
+                {'started':true}
+            );
+
+            // Take the details of the course
+            var course = MM.db.get("courses", courseId);
+
+            // and add them to the my_learning section.
+            MM.db.insert("my_learning", course);
+
+            MM.plugins.findcourses.list_categories();
+        },
+
+        error: function(data) {
+            console.log("Error");
+            console.log(data);
+        }
+    }
+
+    var plugin = {
+        settings: {
+            name: "findcourses",
+            type: "general",
+            lang: {
+                component: "core"
+            },
+            menuURL: "#find_courses"
+        },
+
+        storage: {
+            category: {type: "model"},
+            categories: {type: "collection", model: "category"}
+        },
+
+        routes: [
+            ["find_courses", "find_courses", "list_categories"],
+            ["enrol_user/:courseId", "enrol_user", "enrol_user"]
+        ],
+
+        templates: {
+            "results": {
+                html: coursesTpl
+            }
+        },
+
+        last_enrolled_course: null,
+        last_found_courses:undefined,
+
+        enrol_user: function(courseId) {
+            // RoleId 5 = Student
+            // This format isn't a mistake, the call requires an array
+            // of arrays.
+            var data = {
+                'enrolments':[{
+                    roleid: 5,
+                    userid: MM.site.get('userid'),
+                    courseid: courseId
+                }]
+            };
+
+            MM.plugins.findcourses.last_enrolled_course = courseId;
+
+            MM.moodleWSCall(
+                'enrol_manual_enrol_users',
+                data,
+                callbacks.manual_enrolment_successful,
+                {responseExpected:false},
+                callbacks.error
+            );
+        },
+
+        list_categories: function() {
+            MM.moodleWSCall(
+                'core_course_get_categories',
+                {},
+                callbacks.find_categories_successful,
+                {},
+                callbacks.error
+            );
+        },
+
+        find_courses: function() {
+            if (MM.plugins.findcourses.last_found_courses !== undefined) {
+                callbacks.find_courses_successful(
+                    MM.plugins.findcourses.last_found_courses
+                );
+                MM.plugins.findcourses.last_found_courses = undefined;
+            } else {
+                MM.moodleWSCall(
+                    'core_course_get_courses',
+                    {},
+                    callbacks.find_courses_successful,
+                    {},
+                    callbacks.error
+                );
+            }
+        }
+    };
+
+    MM.registerPlugin(plugin);
+});
