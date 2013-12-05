379f8eb707c8f8e7e8793a03359bd4273718a610
Dominic Orme <dominic.orme@kineo.com>
2013-12-05 09:41:03 +0000

    Feedback plugin additions. Overview page has a submit button to use against the new feedback webservice

--- plugins/feedback/main.js
+++ plugins/feedback/main.js
@@ -1,4 +1,5 @@
 var templates = [
+    "root/externallib/text!root/plugins/feedback/overview.html",
     "root/externallib/text!root/plugins/feedback/info.html",
     "root/externallib/text!root/plugins/feedback/label.html",
     "root/externallib/text!root/plugins/feedback/textarea.html",
@@ -13,7 +14,7 @@ var templates = [
 define(
     templates,
     function(
-        info, label, textarea, multichoice, ratedmultichoice, numeric,
+        overview, info, label, textarea, multichoice, ratedmultichoice, numeric,
         textfield, captcha, pagebreak
     ) {
     var plugin = {
@@ -27,6 +28,7 @@ define(
         },
 
         templates: {
+            'overview' : {'html':overview},
             'info' : {'html':info},
             'label' : {'html':label},
             'textarea' : {'html':textarea},
@@ -39,7 +41,7 @@ define(
         },
 
         routes: [
-            ["feedback/:courseId", "main", "main"]
+            ["feedback/:courseModuleId", "main", "main"]
         ],
 
         // Feedbacks has many feedback
@@ -91,47 +93,122 @@ define(
             $("#panel-right").show();
         },
 
+        currentFeedbackId:undefined,
+
         _getFeedbackQuestionsSuccess: function(data) {
+            var feedback = data.feedback;
             var questions = data.questions;
             var answers = data.answers;
+            MM.plugins.feedback.currentFeedbackId = feedback[0].id
             var html = "";
 
-            _.each(answers, function(answer) {
-                _.each(questions, function(question) {
+            // Save the feedback
+            MM.db.insert('feedbacks', feedback);
+
+            // Go through each question linking it with any available answer
+            _.each(questions, function(question) {
+                _.each(answers, function(answer) {
                     if (question.id === answer.item) {
                         question.answer = answer;
                     }
                 });
+
+                // Whilst going through all the questions, record them for
+                // later use.
+                MM.db.insert('questions', question);
             });
 
-            MM.db.insert('feedback', {'id':_.first(questions).feedback});
-            MM.db.insert('questions', questions);
-
-            MM.plugins.feedback._displayFeedback(questions);
+            MM.plugins.feedback._displayFeedback(feedback.id);
         },
 
-        _displayFeedback: function(questions) {
+        _displayFeedback: function(feedbackId) {
             var html = "";
 
-            _.each(questions, function(question) {
-                console.log("Type: " + question.typ);
-                var x = MM.tpl.render(MM.plugins.feedback.templates[question.typ].html, question);
-                html += x;
-            });
+            html = MM.tpl.render(
+                MM.plugins.feedback.templates.overview.html,
+                {
+                    feedback:{
+                        id:feedbackId
+                    }
+                }
+            );
+
             MM.panels.show("center", html);
+            $(document).find('#submit-feedback').on(
+                'click', MM.plugins.feedback._saveFeedback
+            );
         },
 
         _getFeedbackQuestionsFailure: function() {
-
+            console.log("It died");
         },
 
-        main: function(courseId) {
+        _saveFeedback: function() {
+            var answers = $(document).find(".question-answer");
+            var allRequiredAnswered = true;
+            var response = [];
+            _.each(answers, function(answer) {
+                var answer = $(answer);
+                var value = answer.val();
+                if (value.trim().length === 0) {
+                    value = answer.text();
+                }
+
+                if (answer.data('required') === 1) {
+                    if (value.trim().length === 0) {
+                        allRequiredAnswered = false;
+                        return false;
+                    }
+                }
+                response.push(
+                    {'questionid':answer.data('questionid'), 'answer':value}
+                );
+            });
+            if (!allRequiredAnswered) {
+                console.log("All required answers aren't...");
+            } else {
+                var callBack = MM.plugins.feedback._sendAnswersSuccess;
+                var errorCallBack = MM.plugins.feedback._sendAnswersFailure;
+                var preSets = {};
+                MM.moodleWSCall(
+                    'mod_feedback_send_answers', {'answers':response}, callBack,
+                    preSets, errorCallBack
+                );
+            }
+        },
+
+        _sendAnswersSuccess: function(data) {
+            // All went fine...
+            // Answers is an array of saved or updated answers
+            var answers = data.answers;
+            _.each(answers, function(answer) {
+                var id = answer.id;
+                var qId = answer.questionId;
+                var success = answer.success;
+                var error = answer.error;
+                var value = answer.answer;
+
+                // If we didn't have an error, save the answer to the question.
+                if (error.length === 0) {
+                    var question = MM.db.get('questions', qId);
+                    question.answer.id = id;
+                    question.answer.item = qId;
+                    question.answer.value = value;
+                }
+            });
+        },
+
+        _sendAnswersFailure: function() {
+            // died...
+        },
+
+        main: function(courseModuleId) {
             MM.assignCurrentPlugin(MM.plugins.feedback);
             MM.panels.showLoading("center");
 
             // Get the feedback for the course
             var data = {
-                'courseid':courseId
+                'coursemoduleid':courseModuleId
             };
             var callBack = MM.plugins.feedback._getFeedbackQuestionsSuccess;
             var preSets = {};
@@ -140,6 +217,23 @@ define(
                 'mod_feedback_get_questions', data, callBack, preSets,
                 errorCallBack
             );
+        },
+
+        _displayQuestions: function() {
+            var html = "";
+
+            var questions = MM.db.where(
+                'questions', {'feedback':MM.plugins.feedback.currentFeedbackId}
+            );
+
+            _.each(questions, function(question) {
+                var x = MM.tpl.render(
+                    MM.plugins.feedback.templates[question.get('typ')].html, question.toJSON()
+                );
+                html += x;
+            });
+
+            return html;
         }
     }
 
