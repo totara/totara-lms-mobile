8de9a64a75b01a1772f66b3df22729dba051d8f4
Dominic Orme <dominic.orme@kineo.com>
2014-01-08 16:46:13 +0000

    Initial my bookings plugin

---
+++ plugins/mybookings/main.js
@@ -0,0 +1,130 @@
+var templates = [
+    "root/externallib/text!root/plugins/mybookings/layout.html"
+];
+
+define(templates, function(layoutTpl) {
+    var plugin = {
+        settings: {
+            name: "mybookings",
+            type: "user",
+            lang: {
+                component: "core"
+            },
+            menuURL: "#mybookings"
+        },
+
+        templates: {
+            layout:layoutTpl
+        },
+
+        routes: [
+            ["#mybookings", "main", "main"]
+        ],
+
+        sizes: undefined,
+
+        _getSizes: function() {
+
+            // Default tablet.
+            MM.plugins.mybookings.sizes = {
+                withSideBar: {
+                    center:$(document).innerWidth() - MM.navigation.getWidth(),
+                    left:MM.navigation.getWidth()
+                },
+                withoutSideBar: {
+                    center:"100%",
+                    left:0
+                }
+            };
+
+            if (MM.deviceType === "phone") {
+                MM.plugins.mybookings.sizes = {
+                    withSideBar: {
+                        center:0,
+                        left:0
+                    },
+                    withoutSideBar: {
+                        center:"100%",
+                        left:0
+                    }
+                };
+            }
+        },
+
+        resize: function() {
+            if (MM.plugins.mybookings.sizes == undefined) {
+                MM.plugins.mybookings._getSizes();
+            }
+
+            if (MM.deviceType === "tablet") {
+                if (MM.navigation.visible === true) {
+                    $("#panel-center").css({
+                        'width':MM.plugins.mybookings.sizes.withSideBar.center,
+                        'left':MM.plugins.mybookings.sizes.withSideBar.left
+                    });
+                } else {
+                    $("#panel-center").css({
+                        'width':MM.plugins.mybookings.sizes.withoutSideBar.center,
+                        'left':MM.plugins.mybookings.sizes.withoutSideBar.left
+                    });
+                }
+            } else if (MM.deviceType === "phone") {
+                $("#panel-center").css({
+                    'width':'100%',
+                    'left':0
+                });
+            }
+
+            $("#panel-right").hide();
+        },
+
+        cleanUp: function() {
+            $("#panel-center").html("");
+            $("#panel-right").show();
+        },
+
+        _getBookings: function() {
+            var method = 'core_course_get_courses';
+            MM.moodleWSCall(
+                method,
+                {},
+                MM.plugins.findcourses._getCoursesSuccessful,
+                {},
+                MM.plugins.findcourses._getCoursesFailure
+            );
+
+            var method = "";
+            var data = {};
+            var callback = MM.plugins.mybookings._getBookingsSuccess;
+            var presets = { omitExpires: true, cache: false };
+            var errorCallback = MM.plugins.mybookings._getBookingsFailure;
+            MM.moodleWSCall(method, data, callback, presets, errorCallback);
+        },
+
+        _getBookingsSuccess: function(data) {
+            var values = {
+                'bookings':data
+            };
+            var html = MM.tpl.render(
+                MM.plugins.mybookings.templates.layout, values, {}
+            );
+            MM.panels.show('center', html, {hideRight: true});
+            MM.util.setupAccordion($("#panel-center"));
+            MM.util.setupBackButton();
+        },
+
+        _getBookingsFailure: function() {
+
+        },
+
+        main: function() {
+            MM.assignCurrentPlugin(MM.plugins.mybookings);
+            MM.panels.showLoading("center");
+
+            // Put your code here
+            MM.plugins.mybookings._getBookings();
+        }
+    }
+
+    MM.registerPlugin(plugin);
+});
