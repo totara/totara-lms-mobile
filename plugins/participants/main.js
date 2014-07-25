e3fe6bdfa2030d9724d27df53d06ee10ea73d697
Tony Blundell <tony.blundell@kineo.com>
2014-07-25 16:21:18 +0100

    DEVINTTOTL-389 Non-admin users can see "add a note" button, but get permission error if they try to add a note

--- plugins/participants/main.js
+++ plugins/participants/main.js
@@ -24,7 +24,6 @@ define(templates,function (participantsTpl, participantTpl) {
         routes: [
             ["participants/:courseId", "participants", "showParticipants"],
             ["participant/:courseId/:userId", "participants", "showParticipant"],
-            ["note/:courseId/:userId", "note", "addNote"]
         ],
 
         templates: {
@@ -165,68 +164,6 @@ define(templates,function (participantsTpl, participantTpl) {
             MM.popErrorMessage(statusText);
         },
 
-        addNote: function(e, courseId, userId) {
-            e.preventDefault();
-            var element = $(e.target);
-            var userId = element.data('userid');
-            var courseId = element.data('courseid');
-            var addNote = MM.lang.s("addnote");
-
-            var options = {
-                title: addNote,
-                width: "90%",
-                buttons: {}
-            };
-
-            options.buttons[addNote] = function() {
-                var data = {
-                    "notes[0][userid]" : userId,
-                    "notes[0][publishstate]": 'personal',
-                    "notes[0][courseid]": courseId,
-                    "notes[0][text]": $("#addnotetext").val(),
-                    "notes[0][format]": "1"
-                }
-
-                MM.widgets.dialogClose();
-                MM.moodleWSCall(
-                    'moodle_notes_create_notes',
-                    data,
-                    function(r) {
-                        MM.popMessage(MM.lang.s("noteadded"));
-                    },
-                    {
-                        sync: true,
-                        syncData: {
-                            name: addNote,
-                            description: $("#addnotetext").val().substr(0, 30)
-                        }
-                    }
-                );
-
-                // Refresh the hash url for avoid navigation problems.
-                MM.Router.navigate("participant/" + courseId + "/" + userId);
-            };
-
-            options.buttons[MM.lang.s("cancel")] = function() {
-                MM.Router.navigate("participant/" + courseId + "/" + userId);
-                MM.widgets.dialogClose();
-            };
-
-            var rows = 5;
-            var cols = 5;
-            if (MM.deviceType == "tablet") {
-                rows = 15;
-                cols = 50;
-            }
-
-            var html = '\
-            <textarea id="addnotetext" rows="' + rows + '" cols="' + cols + '"></textarea>\
-            ';
-
-            MM.widgets.dialog(html, options);
-            return false;
-        },
-
         sendMessage: function(e, courseId, userId) {
             e.preventDefault();
             var element = $(e.target);
@@ -337,7 +274,6 @@ define(templates,function (participantsTpl, participantTpl) {
                     MM.db.insert("users", newUser);
                     MM.panels.show('center', html, {title: pageTitle});
 
-                    $("#addnote").on(MM.clickType, MM.plugins.participants.addNote);
                     $("#sendmessage").on(MM.clickType, MM.plugins.participants.sendMessage);
 
                     MM.util.setupBackButton();
